df30fde6e4a4ac076048e60a74c0fb0f9c5d41c4
pjlasl <pablleaf@gmail.com>
2020-06-21 18:30:40 -0500

    phase 2

---
+++ copy.js
@@ -0,0 +1,22 @@
+var copyEmailBtn = document.querySelector('.js-copybtn');
+copyEmailBtn.disabled = !document.queryCommandSupported('copy');
+copyEmailBtn.addEventListener('click', function(event) {
+  // Select the email link anchor text
+  var emailLink = document.querySelector('.fireworks');
+  var range = document.createRange();
+  range.selectNode(emailLink);
+  window.getSelection().addRange(range);
+
+  try {
+    // Now that we've selected the anchor text, execute the copy command
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copy email command was ' + msg);
+  } catch(err) {
+    console.log('Oops, unable to copy');
+  }
+
+  // Remove the selections - NOTE: Should use
+  // removeRange(range) when it is supported
+  window.getSelection().removeAllRanges();
+});
